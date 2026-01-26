0f549ae75c1633ca281b816988563a3084795220
quentinsenahoun-web <quentinsenahoun@gmail.com>
2026-01-26 09:58:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ switcher.addEventListener("change", () => {
         el.textContent = el.getAttribute(`data-${lang}`);
     });
 });
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
+    reveals.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
